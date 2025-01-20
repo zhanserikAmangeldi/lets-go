9645057e66f6ca46da7ca7cd8c7fb2beca60b456
zhanserikAmangeldi <amangeldi.janserik2017@gmail.com>
2025-01-20 18:41:36 +0500

    add golang templates

--- main.go
+++ main.go
@@ -1,44 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello from Home page!"))
-}
-func about(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from About page!"))
-}
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Contact page!"))
-}
-func snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "Displaying specific snippet with id %d", id)
-}
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		// w.WriteHeader(http.StatusMethodNotAllowed)
-		// w.Write([]byte("Method not Allowed"))
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	w.Write([]byte("Create a new snippet..."))
-}
 
 func main() {
 	mux := http.NewServeMux()
